ee363f917fa4b399bd501b9e46a7209964fc70b1
kdrzewiecka <kejtka@wp.pl>
2022-04-26 18:39:03 +0200

    pogoda

---
+++ src/components/WeatherView.vue
@@ -0,0 +1,24 @@
+<template>
+   <div class="weatherView">
+  <h1>Weather</h1>
+   {{weather}}
+   </div>
+
+
+</template>
+
+<script>
+export default {
+    name:"WeatherView",
+    data(){
+      return{
+          weather: "sun",
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style> 
